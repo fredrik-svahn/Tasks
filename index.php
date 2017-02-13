457a85346455e66989b26ab4ebd2f840d2f39bc5
itgsod-fredrik-svahn <fredrik.svahn@elev.it-gymnasiet.se>
2017-02-13 15:03:05 +0100

    Har designat om systemet så att alla tasks lagras i samma table

--- index.php
+++ index.php
@@ -9,11 +9,26 @@
 
 	<body>
 		<h1>Projects</h1>
-		<form action="submit.php" method="post">
+		<?php
+		$servername = "localhost";
+		$dbname = "Projects";
+		$username = "root";
+		$password = "root";
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		if ($conn->connect_error) {
+    		die("Connection failed: " . $conn->connect_error);
+		} 
+		
+		$sql = "SELECT * FROM ProjectNames";
+		$result = mysqli_query($conn, $sql);
+		foreach($result as $project) {
+			echo $project["ProjectName"];
+		}
+		mysqli_close($conn);
+		?>
+		<form method="post">
 			<input type="text" name="name" placeholder="Name your new project"></input>
 			<input type="submit" value="Submit">
-			
-			
 		</form>
 	</body>
 
--- submit.php
+++ submit.php
@@ -8,16 +8,7 @@
     				die("Connection failed: " . $conn->connect_error);
 				} 
 
-				$sql = "CREATE TABLE ".$_POST["name"]." (
-				`id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-				`taskname` CHAR(30),
-				`tasktimer` INT(6)
-				)";
-				if (mysqli_query($conn, $sql)) {
-    				echo "Table ".$_POST["name"]." created successfully";
-				} else {
-    			echo "Error creating table: " . mysqli_error($conn);
-				}
+				$sql = "INSERT INTO ProjectNames (ProjectName) VALUES (".$_POST["name"].")";
 
 				mysqli_close($conn);
 
