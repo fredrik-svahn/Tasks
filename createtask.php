826f102afd9f4ca693b5b0d5c9553b92ae3c200b
fredrik-svahn <fredriksvahn97@gmail.com>
2017-02-13 16:58:45 +0100

    Har fixat så att taskpagen visar upp alla tasks ordnade efter expiry datum

--- createtask.php
+++ createtask.php
@@ -1,3 +1,11 @@
 <?php
+	$servername = "localhost";
+	$dbname = "Projects";
+	$username = "root";
+	$password = "root";
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
+	$sql = 'INSERT INTO Tasks VALUES (`ProjectName`, `TaskName`, `Expire`) '
+
+	mysqli_close($conn); 
 ?>
--- taskpage.php
+++ taskpage.php
@@ -1,16 +1,23 @@
 <?php
 	echo $_GET["name"];
-
-
 	$servername = "localhost";
 	$dbname = "Projects";
 	$username = "root";
 	$password = "root";
 	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	$sql = 'SELECT Id, TaskName, Expire FROM Tasks WHERE ProjectName ='.$_GET["name"].' ORDER BY Expire';
+	$result = mysqli_query($conn, $sql);
+
+	foreach($result as $task) {
+		echo $task["Id"]."	".$task["TaskName"]."	".$task["Expire"];
+	}
+
+	mysqli_close($conn);
 ?>
 
 <form action="createtask.php" method="post">
 <input type="text" name="name" placeholder="Name new task"></input>
-<input type="date" step="1"></input><input type="time"></input>
+<input type="date" name="date"></input><input type="time" name="time"></input>
 <input type="submit" value="Submit">
 </form>
