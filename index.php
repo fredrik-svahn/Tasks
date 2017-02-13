97f718db0553bc818ba513f8a049cf480d561795
itgsod-fredrik-svahn <fredrik.svahn@elev.it-gymnasiet.se>
2017-02-13 13:54:35 +0100

    "Skapa nytt projekt"-knapp
    Har börjat jobba på knappen som ska skapa ett nytt projekt, och en ruta
    där man skriver in namnet på det nya projektet

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
 	<head>
 		<title>Tasks</title>
 		<meta charset="UTF-8">
+		<script src="js/script.js" type="text/javascript"></script>
+		<link href="css/style.css" rel="stylesheet">
+
 	</head>
 
 	<body>
 		<h1>Projects</h1>
+		<a id="createNew" href="" onclick="createNew()">Create new project</a>
+		<input id="createNewField"></input>
 	</body>
 
 </html>
---
+++ js/script.js
@@ -0,0 +1,6 @@
+function createNew() {
+	var button = document.getElementById("createNew");
+	var div = document.createElement("div");
+	var createNewField = documet.getElementById("createNewField");
+	createNewField.style = "display: block";
+}
